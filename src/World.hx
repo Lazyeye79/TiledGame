797e73defaa6fddff44327870951123099dbdff1
zowiehi <zhaugaard@gmail.com>
2015-03-24 13:05:44 -0700

    Cleaned up world class

--- src/World.hx
+++ src/World.hx
@@ -2,27 +2,9 @@ import starling.display.Sprite;
 
 class World extends Sprite{
 
-	public var curStars: Array<Array<Star>>;
-
 	var mapGenerator: GenerateMap;
 	public function new(){
-
-		//mapGenerator = new GenerateMap(world);
 		super();
-	}
-
-	// public function currentStars(){
-	// 	for (x in 0...50){
-	// 		for (y in 0 ... 40){
-	// 			curStars[x][y]
-
-	// 		}
-	// 	}
-	// }
-	// {
-		
-	// }
-
-	
+	}	
 
 }
