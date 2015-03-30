cdce115caa833e000034fcf4e74826c2aed91aa9
Josh Frampton <jrf254@nau.edu>
2015-03-29 20:16:03 -0700

    Updated the fuel bar to only use one image

--- src/Game.hx
+++ src/Game.hx
@@ -85,46 +85,48 @@ class Game extends Sprite{
 		fuel10 = new Image(Root.assets.getTexture("fuel10"));
 		fuel10.x = 100;
 		fuel10.y = 100;
-		
-		fuel9 = new Image(Root.assets.getTexture("fuel9"));
-		fuel9.x = 100;
-		fuel9.y = 100;
 
-		fuel8 = new Image(Root.assets.getTexture("fuel8"));
-		fuel8.x = 100;
-		fuel8.y = 100;
 		
-		fuel7 = new Image(Root.assets.getTexture("fuel7"));
-		fuel7.x = 100;
-		fuel7.y = 100;
-
-		fuel6 = new Image(Root.assets.getTexture("fuel6"));
-		fuel6.x = 100;
-		fuel6.y = 100;
 		
-		fuel5 = new Image(Root.assets.getTexture("fuel5"));
-		fuel5.x = 100;
-		fuel5.y = 100;
+		// fuel9 = new Image(Root.assets.getTexture("fuel9"));
+		// fuel9.x = 100;
+		// fuel9.y = 100;
 
-		fuel4 = new Image(Root.assets.getTexture("fuel4"));
-		fuel4.x = 100;
-		fuel4.y = 100;
+		// fuel8 = new Image(Root.assets.getTexture("fuel8"));
+		// fuel8.x = 100;
+		// fuel8.y = 100;
+		
+		// fuel7 = new Image(Root.assets.getTexture("fuel7"));
+		// fuel7.x = 100;
+		// fuel7.y = 100;
+
+		// fuel6 = new Image(Root.assets.getTexture("fuel6"));
+		// fuel6.x = 100;
+		// fuel6.y = 100;
+		
+		// fuel5 = new Image(Root.assets.getTexture("fuel5"));
+		// fuel5.x = 100;
+		// fuel5.y = 100;
+
+		// fuel4 = new Image(Root.assets.getTexture("fuel4"));
+		// fuel4.x = 100;
+		// fuel4.y = 100;
 	
-		fuel3 = new Image(Root.assets.getTexture("fuel3"));
-		fuel3.x = 100;
-		fuel3.y = 100;
+		// fuel3 = new Image(Root.assets.getTexture("fuel3"));
+		// fuel3.x = 100;
+		// fuel3.y = 100;
 
-		fuel2 = new Image(Root.assets.getTexture("fuel2"));
-		fuel2.x = 100;
-		fuel2.y = 100;
+		// fuel2 = new Image(Root.assets.getTexture("fuel2"));
+		// fuel2.x = 100;
+		// fuel2.y = 100;
 		
-		fuel1 = new Image(Root.assets.getTexture("fuel1"));
-		fuel1.x = 100;
-		fuel1.y = 100;
+		// fuel1 = new Image(Root.assets.getTexture("fuel1"));
+		// fuel1.x = 100;
+		// fuel1.y = 100;
 		
-		fuel0 = new Image(Root.assets.getTexture("fuel0"));
-		fuel0.x = 100;
-		fuel0.y = 100;
+		// fuel0 = new Image(Root.assets.getTexture("fuel0"));
+		// fuel0.x = 100;
+		// fuel0.y = 100;
 
 		ship = new Image(Root.assets.getTexture("ship"));
 		ship.x = fx + 300;
@@ -196,37 +198,48 @@ class Game extends Sprite{
 		updateMap();
 
 		if (fuel > 900 && fuel <= 1000){
+			fuel10.scaleX = 1;
 			rootSprite.addChild(fuel10);
 		}
 		if(fuel > 800 && fuel <= 900){
-			rootSprite.addChild(fuel9);
+			fuel10.scaleX = .9;
+			rootSprite.addChild(fuel10);
 		}
 		if(fuel > 700 && fuel <= 800){
-			rootSprite.addChild(fuel8);
+			fuel10.scaleX = .8;
+			rootSprite.addChild(fuel10);
 		}
 		if(fuel > 600 && fuel <= 700){
-			rootSprite.addChild(fuel7);
+			fuel10.scaleX = .7;
+			rootSprite.addChild(fuel10);
 		}
 		if(fuel > 500 && fuel <= 600){
-			rootSprite.addChild(fuel6);
+			fuel10.scaleX = .6;
+			rootSprite.addChild(fuel10);
 		}
 		if(fuel > 400 && fuel <= 500){
-			rootSprite.addChild(fuel5);
+			fuel10.scaleX = .5;
+			rootSprite.addChild(fuel10);
 		}
 		if(fuel > 300 && fuel <= 400){
-			rootSprite.addChild(fuel4);
+			fuel10.scaleX = .4;
+			rootSprite.addChild(fuel10);
 		}
 		if(fuel > 200 && fuel <= 300){
-			rootSprite.addChild(fuel3);
+			fuel10.scaleX = .3;
+			rootSprite.addChild(fuel10);
 		}
 		if(fuel > 100 && fuel <= 200){
-			rootSprite.addChild(fuel2);
+			fuel10.scaleX = .2;
+			rootSprite.addChild(fuel10);
 		}
 		if(fuel > 0 && fuel <= 100){
-			rootSprite.addChild(fuel1);
+			fuel10.scaleX = .1;
+			rootSprite.addChild(fuel10);
 		}
 		if(fuel == 0){
-			rootSprite.addChild(fuel0);
+			fuel10.scaleX = 0;
+			rootSprite.addChild(fuel10);
 		}
 
 		ship.x += vx;
