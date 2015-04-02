d51c9323efa416b464ac84271a30115346bc3aeb
zowiehi <zhaugaard@gmail.com>
2015-04-01 20:39:00 -0700

    Music

--- src/Root.hx
+++ src/Root.hx
@@ -3,10 +3,17 @@ import starling.utils.AssetManager;
 import starling.core.Starling;
 import starling.animation.Transitions;
 
+import flash.media.SoundChannel;
+import flash.media.SoundTransform;
+
 class Root extends Sprite {
 
 	public static var assets: AssetManager;
 	public var rootSprite:Sprite;
+
+	// Sound variables
+	var musicChannel:SoundChannel;	
+	var transform:SoundTransform;
 
 	public function new() {
 		rootSprite = this;
@@ -33,9 +40,18 @@ class Root extends Sprite {
 		assets.enqueue("assets/fuelcan.png");
 		assets.enqueue("assets/mainM.png");
 
+		assets.enqueue("assets/music.mp3");
+
 		
 		assets.loadQueue(function onProgress(ratio:Float) {
 			if (ratio == 1) {
+
+				//Start the game music
+				musicChannel = assets.playSound("music");
+				musicChannel.addEventListener(flash.events.Event.SOUND_COMPLETE, soundComplete);
+				transform = new SoundTransform(0.3, 0);
+				musicChannel.soundTransform = transform;
+
 				// fade the loading screen, start game
 				Starling.juggler.tween(startup.loadingBitmap, 1.0, {
 					transition:Transitions.EASE_OUT, delay:0.5, alpha: 0, onComplete: function() {
@@ -47,4 +63,13 @@ class Root extends Sprite {
 			}
 		});
 	}
+
+	/** Start the music after it's finished playing */
+	function soundComplete(e:flash.events.Event)
+	{
+		musicChannel = assets.playSound("game_music");
+		musicChannel.addEventListener(flash.events.Event.SOUND_COMPLETE, soundComplete);
+		musicChannel.soundTransform = transform;
+	}
+
 }
