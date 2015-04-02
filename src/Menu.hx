d9dc5514806fe3d0f1ec61e5564fca376cd5d63c
Lazyeye79 <jdh346@gmail.com>
2015-04-02 13:00:16 -0700

    Updated the menu

--- src/Menu.hx
+++ src/Menu.hx
@@ -37,22 +37,20 @@ class Menu extends Sprite{
 	private function setStage(state : GameState)
 	{
 		removeChildren(0);
-		removeChildren(1);
 		addChild(MM);
-		//MM.x = Starling.current.stage.stageWidth;
-		//MM.y = Starling.current.stage.stageHeight;
 		MM.x = 0;
 		MM.y = 0;
 		switch(state)
 		{
 			case Menu:
 				title = new MenuText(350,100,"Space Cruise");
-				title.fontSize = 50;
-				title.y = 50;
+				title.fontSize = 45;
+				title.y = 300;
 				addChild(title);
 
 				var play = new MenuButton("Play");
-				play.y = title.y + 100;
+				play.y = title.y + title.height + 50;
+				play.x = (Starling.current.stage.stageWidth - play.width * 3)/2;
 				play.fontSize = 20;
 				play.addEventListener(Event.TRIGGERED,
 				function() {
@@ -61,51 +59,59 @@ class Menu extends Sprite{
 				addChild(play);
 
 				var instr = new MenuButton("Instructions");
-				instr.y = play.y + 100;
+				instr.y = play.y;
+				//instr.x = play.x + play.width;
 				instr.fontSize = 20;
 				instr.addEventListener(Event.TRIGGERED,
 				function(){	setStage(Instructions);});
 				addChild(instr);
 
 				var cred = new MenuButton("Credits");
-				cred.y = instr.y + 100;
+				cred.y = play.y;
+				cred.x = cred.x + cred.width;
 				cred.fontSize = 20;
 				cred.addEventListener(Event.TRIGGERED,
 				function(){	setStage(Credits);});
 				addChild(cred);
 
 			case Instructions:
-				title = new MenuText(200,200,"How To Play");
-				title.fontSize = 50;
-				title.y = 0;
+				title = new MenuText(300,75,"How To Play");
+				title.fontSize = 45;
+				title.y = 300;
 				addChild(title);
 
-				var back = new MenuButton("Back");
-				back.y = title.y + 200;
-				back.fontSize = 30;
-				back.addEventListener(Event.TRIGGERED,
-				function(){ setStage(Menu);});
-				addChild(back);
-
-				var instr = new MenuText(400, 400, "Add instructions here");
+				var instr = new MenuText(350, 200, "Move the 'Ship' with WASD or Arrow Keys. Collect the missing space passengers while avoiding the asteroids scattered about. But don't forget to watch your fuel.");
 				instr.fontSize = 18;
 				instr.x = Starling.current.stage.stageWidth/2 - instr.width/2;
-				instr.y = back.y + 75;
+				instr.y = title.y + title.height/2;
 				addChild(instr);
 
-			case Credits:
 				var back = new MenuButton("Back");
-				back.fontSize = 20; back.y = 50;
+				back.fontSize = 30;
+				back.y = instr.y + instr.height;
 				back.addEventListener(Event.TRIGGERED,
 				function(){ setStage(Menu);});
 				addChild(back);
 
-				var cred = new MenuText(300,300,"Credits\n--------"+
-				"\nJordan Harris\nCherie Parsons\nAdd rest of the team member names");
+			case Credits:
+				title = new MenuText(350,75,"We Made This");
+				title.fontSize = 45;
+				title.y = 300;
+				addChild(title);
+
+				var cred = new MenuText(300,200,"Credits\n--------"+
+				"\nJordan Harris\tCherie Parsons\nErin Bailey\tTeam Member\nTeam Member\tTeam Member");
 				cred.fontSize = 20;
-				cred.y = back.y + 200;
+				cred.y = title.y + title.height/2;
 				cred.x = Starling.current.stage.stageWidth/2 - cred.width/2;
 				addChild(cred);
+				
+				var back = new MenuButton("Back");
+				back.fontSize = 30;
+				back.y = cred.y + cred.height;
+				back.addEventListener(Event.TRIGGERED,
+				function(){ setStage(Menu);});
+				addChild(back);
 		}
 	}
 
@@ -132,7 +138,7 @@ class MenuText extends TextField
 		addEventListener(Event.ADDED_TO_STAGE, function()
 		{
 			var tweenx = x;
-			x -= 700;
+			x += 700;
 			Starling.juggler.tween(this, 0.25,
 			{
 				transition: Transitions.LINEAR,
